f50b2e8e32a2838f8ac22bc38ac0cae16b4d4f40
PrinceFroggy <asolesa@yahoo.ca>
2017-03-14 12:30:32 -0400

    Initial Commit

---
+++ Autolayout 2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Autolayout 2
+//
+//  Created by Andrew Solesa on 2017-03-14.
+//  Copyright © 2017 KSG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
